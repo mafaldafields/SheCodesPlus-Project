8e2d8ea40f5d075b761ac56f2b2da83280c88c84
mafaldafields <mafalda.fields@outlook.pt>
2022-10-15 17:19:19 +0100

    get current date and time

--- src/java.js
+++ src/java.js
@@ -1,3 +1,19 @@
+// Get dates
+let now = new Date();
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
+let weekDay = weekDays[now.getDay()];
+let day = now.getDate();
+let allMonths = ["January", "February", "March", "April", "May", "June", "July","August", "September", "October", "November", "December"];
+let month = allMonths[now.getMonth()];
+let year = now.getFullYear();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let date = document.querySelector("#fontdate");
+date.innerHTML=`${weekDay}, ${day} ${month} ${year}`;
+let time = document.querySelector("#fonttime");
+time.innerHTML=`${hour}:${minutes}`;
+
+
 // Weather API Integration - axios has been firstly installed
 let apiKey ="f2ef2a14fcdd5a099883dfccadbdb334";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&units=metric&appid=f2ef2a14fcdd5a099883dfccadbdb334`;
