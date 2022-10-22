c51e68ac29f58d22e18ba81653c451668397607c
mafaldafields <mafalda.fields@outlook.pt>
2022-10-22 21:33:48 +0100

    form & api for city submitted

--- src/java.js
+++ src/java.js
@@ -19,7 +19,6 @@ function displayTime () {
     }}
     displayTime();
 
-
 // DISPLAY WEEKDAYS
     // today
 let today = weekDays[now.getDay()];
@@ -37,8 +36,7 @@ function showTodayPlus1 () {
         let todayplus1_inv = weekDays[inverse1];
         let todayplus1T = document.querySelector("#todayplus1");
         todayplus1T.innerHTML = `${todayplus1_inv}`
-    }
-}
+    }}
     // today plus 2
     let indexplus2=[now.getDay()]-7+2;
     function showTodayPlus2() {
@@ -51,8 +49,7 @@ function showTodayPlus1 () {
             let todayplus2_inv = weekDays[inverse2];
             let todayplus2T=document.querySelector("#todayplus2");
             todayplus2T.innerHTML=`${todayplus2_inv}`
-        }
-    }
+        }}
     //today plus 3
     let indexplus3=[now.getDay()]-7+3;
     function showTodayPlus3 () {
@@ -65,136 +62,109 @@ function showTodayPlus1 () {
             let todayplus3_inv=weekDays[inverse3];
             let todayplus3T=document.querySelector("#todayplus3");
             todayplus3T.innerHTML=`${todayplus3_inv}`;
-        }
-    }
-
+        }}
     //Run all functions for all weekDays
     showTodayPlus1();
     showTodayPlus2();
     showTodayPlus3();
 
-//CALCULATE CURRENT LOCATION
-    function currentLocation (position) { //to make sure it is working, this function isn't really doing anything useful for the project
-        let currentLong = position.coords.longitude;
-        let currentLat = position.coords.latitude;
-    }
-    navigator.geolocation.getCurrentPosition(currentLocation);
-
 // AUTO - DISPLAY WEATHER & FORECAST FOR CURRENT LOCATION
+function displayResponse(response) {
+    let currentTemp=Math.round(response.data.list[0].main.temp);
+    let currentTempText=document.querySelector(".temperatureToday");
+    currentTempText.innerHTML=`${currentTemp}ºC`;
+     let windspeedT = document.querySelector(".windspeed");
+    let windspeed = response.data.list[0].wind.speed;
+    let currentTempDesc=response.data.list[0].weather[0].description;
+    let currentTempDescription=currentTempDesc.charAt(0).toUpperCase()+currentTempDesc.slice(1);
+    let temperatureTodayDescription=document.querySelector(".temperatureTodayDescription");
+    temperatureTodayDescription.innerHTML=`${currentTempDescription}`;
+    let todayIconCode=response.data.list[0].weather[0].icon;
+    windspeedT.innerHTML=`Wind speed is ${windspeed}m/s`;
+    let todayIconT=document.querySelector("#todayIcon");
+    todayIconT.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${todayIconCode}@2x.png`);
+    //todayplus1
+    let tempTodayplus1 = Math.round(response.data.list[8].main.temp);
+    let tempTodayplus1T = document.querySelector (".temperatureTodayplus1");
+    tempTodayplus1T.innerHTML=`${tempTodayplus1}ºC`;
+    let windspeedTplus1 = document.querySelector(".windspeedtodayplus1");
+    let windspeedplus1 = response.data.list[8].wind.speed;
+    windspeedTplus1.innerHTML=`Wind speed will be ${windspeedplus1}m/s`
+    let currentTempDescplus1=(response.data.list[8].weather[0].description);
+    let currentTempDescriptionplus1=currentTempDescplus1.charAt(0).toUpperCase()+currentTempDescplus1.slice(1);
+    let temperatureTodayDescriptionplus1=document.querySelector(".temperatureTodayDescriptionplus1");
+    temperatureTodayDescriptionplus1.innerHTML=`${currentTempDescriptionplus1}`;
+    let todayIconCodeplus1=response.data.list[8].weather[0].icon;
+    let todayIconTplus1=document.querySelector("#todayIconplus1");
+    todayIconTplus1.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${todayIconCodeplus1}.png`);
+    //todayplus2
+    let tempTodayplus2 = Math.round(response.data.list[16].main.temp);
+    let tempTodayplus2T = document.querySelector (".temperatureTodayplus2");
+    tempTodayplus2T.innerHTML=`${tempTodayplus2}ºC`;
+    let windspeedTplus2 = document.querySelector(".windspeedtodayplus2");
+    let windspeedplus2 = response.data.list[16].wind.speed;
+    windspeedTplus2.innerHTML=`Wind speed will be ${windspeedplus2}m/s`
+    let currentTempDescplus2=(response.data.list[16].weather[0].description);
+    let currentTempDescriptionplus2=currentTempDescplus2.charAt(0).toUpperCase()+currentTempDescplus2.slice(1);
+    let temperatureTodayDescriptionplus2=document.querySelector(".temperatureTodayDescriptionplus2");
+    temperatureTodayDescriptionplus2.innerHTML=`${currentTempDescriptionplus2}`;
+    let todayIconCodeplus2=response.data.list[16].weather[0].icon;
+    let todayIconTplus2=document.querySelector("#todayIconplus2");
+    todayIconTplus2.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${todayIconCodeplus2}.png`);
+    //todayplus3
+    let tempTodayplus3 = Math.round(response.data.list[24].main.temp);
+    let tempTodayplus3T = document.querySelector (".temperatureTodayplus3");
+    tempTodayplus3T.innerHTML=`${tempTodayplus3}ºC`;
+    let windspeedTplus3 = document.querySelector(".windspeedtodayplus3");
+    let windspeedplus3 = response.data.list[24].wind.speed;
+    windspeedTplus3.innerHTML=`Wind speed will be ${windspeedplus3}m/s`
+    let currentTempDescplus3=(response.data.list[24].weather[0].description);
+    let currentTempDescriptionplus3=currentTempDescplus3.charAt(0).toUpperCase()+currentTempDescplus3.slice(1);
+    let temperatureTodayDescriptionplus3=document.querySelector(".temperatureTodayDescriptionplus3");
+    temperatureTodayDescriptionplus3.innerHTML=`${currentTempDescriptionplus3}`;
+    let todayIconCodeplus3=response.data.list[24].weather[0].icon;
+    let todayIconTplus3=document.querySelector("#todayIconplus3");
+    todayIconTplus3.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${todayIconCodeplus3}.png`);
+
+        let displayinfocity = document.querySelector("#displayinfocity");
+        let cityforecasted = response.data.city.name;
+        let countryforecasted = response.data.city.country;
+        displayinfocity.innerHTML=`Displaying forecast for ${cityforecasted}, ${countryforecasted}`;
+};
+
 function getCPWeather(position) { // get current position weather || auto
-    let currentLong = position.coords.longitude;
-    let currentLat = position.coords.latitude;
-    console.log(currentLong);
-    console.log(currentLat);
+    let Long = position.coords.longitude;
+    let Lat = position.coords.latitude;
     let apiKey =`8648397279af896dbfe0e9040102c5d4`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${currentLat}&lon=${currentLong}&appid=${apiKey}&units=metric`;
-        function displayResponse(response) {
-            let currentTemp=Math.round(response.data.list[0].main.temp);
-            let currentTempText=document.querySelector(".temperatureToday");
-            currentTempText.innerHTML=`${currentTemp}ºC`;
-             let windspeedT = document.querySelector(".windspeed");
-            let windspeed = response.data.list[0].wind.speed;
-            let currentTempDesc=response.data.list[0].weather[0].description;
-            let currentTempDescription=currentTempDesc.charAt(0).toUpperCase()+currentTempDesc.slice(1);
-            let temperatureTodayDescription=document.querySelector(".temperatureTodayDescription");
-            temperatureTodayDescription.innerHTML=`${currentTempDescription}`;
-            let todayIconCode=response.data.list[0].weather[0].icon;
-            windspeedT.innerHTML=`Wind speed is ${windspeed}m/s`;
-            let todayIconT=document.querySelector("#todayIcon");
-            todayIconT.setAttribute(
-                "src",
-                `https://openweathermap.org/img/wn/${todayIconCode}@2x.png`
-            );
-           
-            //todayplus1
-            let tempTodayplus1 = Math.round(response.data.list[8].main.temp);
-            let tempTodayplus1T = document.querySelector (".temperatureTodayplus1");
-            tempTodayplus1T.innerHTML=`${tempTodayplus1}ºC`;
-            let windspeedTplus1 = document.querySelector(".windspeedtodayplus1");
-            let windspeedplus1 = response.data.list[8].wind.speed;
-            windspeedTplus1.innerHTML=`Wind speed will be ${windspeedplus1}m/s`
-            let currentTempDescplus1=(response.data.list[8].weather[0].description);
-            let currentTempDescriptionplus1=currentTempDescplus1.charAt(0).toUpperCase()+currentTempDescplus1.slice(1);
-            let temperatureTodayDescriptionplus1=document.querySelector(".temperatureTodayDescriptionplus1");
-            temperatureTodayDescriptionplus1.innerHTML=`${currentTempDescriptionplus1}`;
-            let todayIconCodeplus1=response.data.list[8].weather[0].icon;
-            let todayIconTplus1=document.querySelector("#todayIconplus1");
-            todayIconTplus1.setAttribute(
-                "src",
-                `https://openweathermap.org/img/wn/${todayIconCodeplus1}.png`
-            );
-
-            //todayplus2
-            let tempTodayplus2 = Math.round(response.data.list[16].main.temp);
-            let tempTodayplus2T = document.querySelector (".temperatureTodayplus2");
-            tempTodayplus2T.innerHTML=`${tempTodayplus2}ºC`;
-            let windspeedTplus2 = document.querySelector(".windspeedtodayplus2");
-            let windspeedplus2 = response.data.list[16].wind.speed;
-            windspeedTplus2.innerHTML=`Wind speed will be ${windspeedplus2}m/s`
-            let currentTempDescplus2=(response.data.list[16].weather[0].description);
-            let currentTempDescriptionplus2=currentTempDescplus2.charAt(0).toUpperCase()+currentTempDescplus2.slice(1);
-            let temperatureTodayDescriptionplus2=document.querySelector(".temperatureTodayDescriptionplus2");
-            temperatureTodayDescriptionplus2.innerHTML=`${currentTempDescriptionplus2}`;
-            let todayIconCodeplus2=response.data.list[16].weather[0].icon;
-            let todayIconTplus2=document.querySelector("#todayIconplus2");
-            todayIconTplus2.setAttribute(
-                "src",
-                `https://openweathermap.org/img/wn/${todayIconCodeplus2}.png`
-            );
-
-            //todayplus3
-            let tempTodayplus3 = Math.round(response.data.list[24].main.temp);
-            let tempTodayplus3T = document.querySelector (".temperatureTodayplus3");
-            tempTodayplus3T.innerHTML=`${tempTodayplus3}ºC`;
-            let windspeedTplus3 = document.querySelector(".windspeedtodayplus3");
-            let windspeedplus3 = response.data.list[24].wind.speed;
-            windspeedTplus3.innerHTML=`Wind speed will be ${windspeedplus3}m/s`
-            let currentTempDescplus3=(response.data.list[24].weather[0].description);
-            let currentTempDescriptionplus3=currentTempDescplus3.charAt(0).toUpperCase()+currentTempDescplus3.slice(1);
-            let temperatureTodayDescriptionplus3=document.querySelector(".temperatureTodayDescriptionplus3");
-            temperatureTodayDescriptionplus3.innerHTML=`${currentTempDescriptionplus3}`;
-            let todayIconCodeplus3=response.data.list[24].weather[0].icon;
-            let todayIconTplus3=document.querySelector("#todayIconplus3");
-            todayIconTplus3.setAttribute(
-                "src",
-                `https://openweathermap.org/img/wn/${todayIconCodeplus3}.png`
-            );
-        };
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${Lat}&lon=${Long}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayResponse);
 }
- navigator.geolocation.getCurrentPosition(getCPWeather) 
+ navigator.geolocation.getCurrentPosition(getCPWeather); 
 
-/
-
-
-
-//need to review under
-//Calculate current location after clicking current location button
-function calculateCurrentCity (event) {
+function weatherForSearched (event) {
     event.preventDefault();
-    function getResponse(position) {
-    let currentLong = position.coords.longitude;
-    let currentLat = position.coords.latitude;
-    console.log(currentLat);
-    console.log(currentLong);
-    }
-    navigator.geolocation.getCurrentPosition(getResponse);
+    let searchedCity = document.querySelector("#search-input");
+    let apiKey =`8648397279af896dbfe0e9040102c5d4`;
+    let apiUrl1 = `http://api.openweathermap.org/geo/1.0/direct?q=${searchedCity.value}&appid=${apiKey}`
+        function defineL (response){
+            let Long = response.data[0].lon;
+            let Lat = response.data[0].lat;
+            let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${Lat}&lon=${Long}&appid=${apiKey}&units=metric`;
+            axios.get(apiUrl).then(displayResponse);
+        }
+    axios.get(apiUrl1).then(defineL);
 }
 
-function showCurrentCityWeather(event){
-    event.preventDefault();
-    
-}
+let searchForm = document.querySelector ("#search-bar");
+searchForm.addEventListener("submit", weatherForSearched)
 
-//let lat = "Lisbon";// to change! 
-//let apiUrl = `https://api.openweathermap.org/data/2.5/forecast'lat=${lat}&${lon}={lon}&units=metric&appid=f2ef2a14fcdd5a099883dfccadbdb334`;
-//function displayResponse (response) {
-    //console.log(response.data)
-    //let temperatureToday = document.querySelector(".temperatureToday");
-    //let temperatureTodayAPI=Math.round(response.data.main.temp);
-    //temperatureToday.innerHTML=`${temperatureTodayAPI}ºC`;
-//axios.get(apiUrl).then(displayResponse);
 
-let currentCityButton=document.querySelector("#currentLocationButton");
-currentCityButton.addEventListener("click", calculateCurrentCity);
-currentCityButton.addEventListener("click", showCurrentCityWeather);
+
